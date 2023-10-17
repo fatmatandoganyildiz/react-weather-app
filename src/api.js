13f6ecfb684269106ec373841f59619821a0d7ab
Fatma Tandogan Yildiz <fatmatandogan609@gmail.com>
2023-10-17 09:11:07 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,15 @@
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6a2b165e57mshf1fe2e2265ac103p1ccdaajsn7aae1daaf79d",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "8dde97c5f28f92504124cff65ef1ddc5";
+
